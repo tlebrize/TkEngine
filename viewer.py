82c38339dfc967a4b919b5926a17b29f6b2ea302
Timothe LEBRIZE <tlebrize@e1r2p19.42.fr>
2017-05-16 15:24:16 +0200

    youpi

---
+++ viewer.py
@@ -0,0 +1,79 @@
+import pyglet, sys, os, collections
+from pyglet.window import key
+
+class Viewer(pyglet.window.Window):
+	def __init__(self):
+		super(Viewer, self).__init__(800, 800, resizable=True, )
+		self.format = ['.png', '.jpg', '.gif']
+		i = 0
+		self.files = collections.OrderedDict({sys.argv[1] + '/' + filename: None
+			for filename in os.listdir(sys.argv[1]) if filename[-4:] in self.format})
+		for filename in self.files:
+			print(i, ':', filename)
+			i += 1
+		self.current_index = 0
+		self.current = None
+		self.scale = 1
+		self.position = (self.width // 2, self.height // 2)
+		self.load()
+		self.placed = []
+
+	def load(self):
+		filename = list(self.files)[self.current_index]
+		if self.current:
+			self.position = (self.current.x, self.current.y)
+			self.scale = self.current.scale
+		if self.files[filename] is not None:
+			self.current = self.files[filename]
+		else:
+			if '.gif' in filename[-4:]:
+				animation = pyglet.image.load_animation(filename)
+				frames = pyglet.image.atlas.TextureBin()
+				animation.add_to_texture_bin(frames)
+				self.current = pyglet.sprite.Sprite(animation)
+			else:
+				image = pyglet.image.load(filename)
+				self.current = pyglet.sprite.Sprite(image)
+		self.set_caption(filename)
+		self.current.x = self.position[0]
+		self.current.y = self.position[1]
+		self.current.scale = self.scale
+
+	def on_draw(self):
+		self.clear()
+		for sprite in self.placed:
+			sprite.draw()
+		self.current.draw()
+
+	def on_key_press(self, symbol, modifier):
+		if symbol in [48 + i for i in range(0, 10)]:
+			self.current_index = (symbol - 48) % len(self.files)
+			self.load()
+		elif symbol == key.TAB:
+			self.current_index = (self.current_index + 1) % len(self.files)
+			self.load()
+		elif symbol == key.ESCAPE:
+			self.clear()
+			exit()
+		elif symbol == key.Q:
+			self.current.scale += 0.1
+		elif symbol == key.W:
+			self.current.scale -= 0.1
+		elif symbol == key.UP:
+			self.current.y += 25
+		elif symbol == key.DOWN:
+			self.current.y -= 25
+		elif symbol == key.RIGHT:
+			self.current.x += 25
+		elif symbol == key.LEFT:
+			self.current.x -= 25
+		elif symbol == key.SPACE:
+			self.placed.append(self.current)
+			self.current = None
+			self.load()
+
+
+if __name__ == '__main__':
+	win = Viewer()
+	pyglet.app.run()
+
---
+++ viewer.spec
@@ -0,0 +1,14 @@
+SPACE
+	place current
+0 1 2 3 4 5 6 7 8 9
+	switch current
+UP DOWN LEFT RIGHT
+	move current
+RETURN
+	save
+screen resize
+	resize screen
+Q W
+	reisze current
+ESCAPE
+	quit
